374f4e5e2819acb3cb698b5c166f4ecc6564e110
jmestd <jmestd00@estudiantes.unileon.es>
2025-01-13 19:27:10 +0100

    Copias de seguridad activadas y ruta a ellas creada automáticamente

--- src/main/org/HappyBank/controller/DatabaseBackUpController.java
+++ src/main/org/HappyBank/controller/DatabaseBackUpController.java
@@ -1,13 +1,15 @@
 package org.HappyBank.controller;
 
 import org.HappyBank.model.BankService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.util.Properties;
 
 public class DatabaseBackUpController {
-
+    private static final Logger logger = LogManager.getLogger(DatabaseBackUpController.class);
     private boolean backUp = false;
     private BankService bankService = new BankService();
 
@@ -22,9 +24,10 @@ public class DatabaseBackUpController {
      * Método que realiza una copia de seguridad de la base de datos si está activo en el .config.
      */
     public void doBackUp() {
+        ensureBackupDirectoryExists();
         readConfig();
         if (backUp) {
-            //bankService.createBackup();
+            bankService.createBackup();
         }
     }
 
@@ -42,9 +45,22 @@ public class DatabaseBackUpController {
                 backUp = Boolean.parseBoolean(prop.getProperty("bbddBackup"));
             }
         } catch (Exception e) {
-            e.printStackTrace();
+            logger.error("Error al leer el archivo de configuración: " + e.getMessage());
         }
     }
-
-
+    
+    /**
+     * Garantiza que la carpeta backup carpeta existe.
+     * <br>
+     * Si no existe, la crea.
+     *
+     */
+    private void ensureBackupDirectoryExists() {
+        File directory = new File("/backup/");
+        if (!directory.exists()) {
+            if (!directory.mkdirs()) {
+                throw new RuntimeException("No se pudo crear la carpeta de respaldo en: " + "/etc/backup/");
+            }
+        }
+    }
 }
